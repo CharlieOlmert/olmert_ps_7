237d5cd02b5616714b33507d361dbddddce91776
Charlie Olmert <charlie@Charlies-MacBook.local>
2018-11-20 21:28:15 -0500

    commit with clean data and working app

--- ps_7/app.R
+++ ps_7/app.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(tidyverse)
+library(ggrepel)
 
 # reading in data from ps_7.rmd
 read_data <- read_rds("error_data")
@@ -17,35 +18,119 @@ read_data <- read_rds("error_data")
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("NYT/Upshot Polling Errors in Different Age Groups, 2018 Midterms"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a dropdown input for age range and a checbox for line of best fit
    sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+     sidebarPanel(
+       # age ranges and choice to show line of best fit
+       selectInput("age", "Choose an Age Range:",
+                   c("18-34" = "youngest",
+                     "35-49" = "younger",
+                     "50-64" = "older",
+                     "65+" = "oldest")),
+       checkboxInput("best_fit", label = "Line of Best Fit", value = FALSE),
+       
+       hr()
+       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("agePlot")
       )
    )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$agePlot <- renderPlot({
+     
+     # a series of if statements which show different graphs in the app depending 
+     # on the age group that the user chooses. Also toggles whether or not to show the 
+     # line of best fit. I discovered "if" statements in Ms. Fridkin's app, which we went over
+     # in class. They have been EXTREMELY helpful.
+     
+     if (input$age == "youngest")
+     {
+       yngst_plot <- read_data %>% 
+         ggplot(aes(x = yngst_per, y = accuracy, color = win_party)) +
+         geom_point() +
+         # labels
+         labs(x = "Percentage of sample aged between 18 and 34", 
+              y = "Accuracy of Poll", color = "Winning Party") +
+         # linking the Democrat with the color blue and the Republican Races with red
+         scale_color_manual(values=c("#0000FF", "#FF0000")) 
+       
+       print(yngst_plot)
+       
+       if (input$best_fit == TRUE) {
+         # creates a straight line of best fit with no wide range around it.
+         bf_line <- yngst_plot + geom_smooth(method = lm, se = FALSE)
+         print(bf_line)
+       }
+     }
+     
+     else if (input$age == "younger")
+     {
+       yngr_plot <- read_data %>% 
+         ggplot(aes(x = yngr_per, y = accuracy, color = win_party)) +
+         geom_point() +
+         # labels
+         labs(x = "Percentage of sample aged between 35 and 49", 
+              y = "Accuracy of Poll", color = "Winning Party") +
+         # linking the Democrat with the color blue and the Republican Races with red
+         scale_color_manual(values=c("#0000FF", "#FF0000"))
+       
+       print(yngr_plot)
+       
+       if (input$best_fit == TRUE) {
+         # creates a straight line of best fit with no wide range around it.
+         bf_line <- yngr_plot + geom_smooth(method = lm, se = FALSE)
+         print(bf_line)
+       }
+     }
+     
+     else if (input$age == "older")
+     {
+       oldr_plot <- read_data %>% 
+         ggplot(aes(x = oldr_per, y = accuracy, color = win_party)) +
+         geom_point() +
+         # labels
+         labs(x = "Percentage of sample aged between 50 and 64", 
+              y = "Accuracy of Poll", color = "Winning Party") +
+         # linking the Democrat with the color blue and the Republican Races with red
+         scale_color_manual(values=c("#0000FF", "#FF0000"))
+       
+       print(oldr_plot)
+       
+       if (input$best_fit == TRUE) {
+         # creates a straight line of best fit with no wide range around it.
+         bf_line <- oldr_plot + geom_smooth(method = lm, se = FALSE)
+         print(bf_line)
+       }
+     }
+     
+     else if (input$age == "oldest")
+     {
+       oldst_plot <- read_data %>% 
+         ggplot(aes(x = oldst_per, y = accuracy, color = win_party)) +
+         geom_point() +
+         # labels
+         labs(x = "Percentage of sample older than 65", 
+              y = "Accuracy of Poll", color = "Winning Party") +
+         # linking the Democrat with the color blue and the Republican Races with red
+         scale_color_manual(values=c("#0000FF", "#FF0000"))
+       
+       print(oldst_plot)
+       
+       if (input$best_fit == TRUE) {
+         # creates a straight line of best fit with no wide range around it.
+         bf_line <- oldst_plot + geom_smooth(method = lm, se = FALSE)
+         print(bf_line)
+       }
+     }
    })
 }
 
